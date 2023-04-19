c14d73e2c1702b8f054bbfe97f7c3683b90393c4
pc03 <pc03@pc03>
2023-04-19 14:45:20 +0900

    연습 커밋

---
+++ JavaStudy/src/ManagementsSystem.java
@@ -0,0 +1,21 @@
+
+public class ManagementsSystem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/******************************
+		 * 
+		 * 클래스명 | ManagementsSystem
+		 * 
+		 * 내용 | DB관리 시스템 메인
+		 * 
+		 * 작성일 | 2023. 04. 19
+		 * 소속 | NEXT_IT 
+		 * 성명 | 박창연
+		 * 
+		 ******************************/
+		
+	}
+
+}
